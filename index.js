615ec1af5a520cb6fc16dbc19439591314876a2e
Fahad-anncode <108000260+Fahad-anncode@users.noreply.github.com>
2023-08-31 22:58:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json()); // Parse JSON request bodies
+
+// Configure Nodemailer transport
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'fahadanncode@gmail.com',
+    pass: 'ptavtrxywlaqsunl'
+  }
+  
+});
+
+// Define the route to handle form submission
+app.post('/send-email', (req, res) => {
+     console.log(req.body)
+  const { name,lastname, email, phone, text } = req.body;
+
+  const mailOptions = {
+    from: 'email',
+    to: 'fahadmasood432@gmail.com',
+    subject: 'About the company',
+    text: `
+      Name: ${name}
+      Lastname: ${lastname}
+      Email: ${email}
+      Phone: ${phone}
+      Text: ${text}
+    `
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+
+    if (error) {
+      console.log('Error sending email:', error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent:', info.response);
+      res.send('Email sent');
+    }
+  });
+});
+
+// Start the server
+app.listen(3001, () => {
+  console.log('Server started on port 3001');
+});
